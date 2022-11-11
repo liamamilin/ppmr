8b2c9317e445bdc706222680d203d1cb26b0ae63
LinMi <15527504293@163.com>
2022-11-11 14:44:10 +0800

    update

--- R/TraceBucketing.R
+++ R/TraceBucketing.R
@@ -23,14 +23,34 @@
 # purrr
 
 
-# 1 single bucketing
+# 1 single bucketing ： 就是不拆分
 # 2 KNN 计算prefix trace 得k近邻，然后用K近邻来构建模型，因此这种方法是动态的，每一个预测都重新训练模型
-# 3 cluster
+# 3 cluster ： 根据trace 进行聚类
 # 4 Prefix length ：根据不同的prefix trace 长度进行划分
 # 5 state ：流程模型中每一个状态构建一个分类器
 # 6 domain knowledge ：根据专家的经验手动进行划分
 
 
-knn_bucketing <- function(prefix_evenLog,new_prefix_trace,n){
+# 输入是
+cluster_bucketing <- function(){
 
 }
+
+
+
+prefix_len_bucketing <- function(){
+
+}
+
+patients %>% group_by_case() %>% summarise(x =paste0(handling,collapse = ""))
+
+
+patients %>% group_by_case() %>% mutate(x =paste0(handling,collapse = ""))
+
+
+
+data <- data.frame(foo=c(1, 1, 2, 3, 3, 3), bar=c('a', 'b', 'a', 'b', 'c', 'd'))
+
+data %>%
+  group_by(foo) %>%
+  mutate(bars_by_foo = paste0(bar, collapse = ""))
